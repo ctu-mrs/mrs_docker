aad7e40f0434d87c290788244e649a7361a27226
Tomas Baca <klaxalk@gmail.com>
2024-12-03 14:32:54 +0100

    generalizing workspace builder

--- catkin_workspace_builder/build_image.sh
+++ catkin_workspace_builder/build_image.sh
@@ -33,7 +33,7 @@ echo "$0: building the user's workspace"
 echo ""
 
 # this first build compiles the contents of "src" and storest the intermediate
-tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker build - --no-cache --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --file Dockerfile --platform=linux/$ARCH
+tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker build - --no-cache --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --file Dockerfile --platform=linux/$ARCH
 
 echo ""
 echo "$0: packing the workspace into a docker image"
@@ -41,7 +41,7 @@ echo ""
 
 # this second build takes the resulting workspace and storest in in a final image
 # that can be deployed to a drone
-docker build . --no-cache --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --tag $OUTPUT_IMAGE --platform=linux/$ARCH
+docker build . --no-cache --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --tag $OUTPUT_IMAGE --platform=linux/$ARCH
 
 echo ""
 echo "$0: workspace was packed into '$OUTPUT_IMAGE'"
--- catkin_workspace_builder/common_vars.sh
+++ catkin_workspace_builder/common_vars.sh
@@ -1,5 +1,10 @@
+# tag for the source image agains which the catkin workspace will be built
 export BASE_IMAGE=ctumrs/mrs_uav_system:1.5.0
 
+# tag for the 'transport' image used for packing the workspace
+export TRANSPORT_IMAGE=alpine:latest
+
+# tag for the resulting image in which the workspace will be packged
 export OUTPUT_IMAGE=catkin_workspace:1.0.0
 
 # location for the exported docker images using the `./export_image.sh` script
