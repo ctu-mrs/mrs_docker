c73ddcc905b456349521f164589207b6961f18a6
Tomas Baca <Tomas Baca>
2025-01-24 11:48:15 +0100

    fixed docker builder multiplatform caching

--- catkin_workspace_builder/build_image.sh
+++ catkin_workspace_builder/build_image.sh
@@ -26,14 +26,26 @@ source ./common_vars.sh
 
 PASS_TO_DOCKER_BUILD="Dockerfile src ${CACHE_PATH}/${WORKSPACE_PATH}"
 
-docker buildx use default
+# multiplatform builder
+BUILDER=container-builder
+
+# get info about an existing builder
+container_builder_info=$(docker buildx inspect ${BUILDER})
+
+if [[ "$?" == "0" ]]; then
+  # activate the builder if it exists
+  docker buildx use ${BUILDER}
+else
+  # create the builder if it does not exist
+  docker buildx create --name ${BUILDER} --driver docker-container --bootstrap --use
+fi
 
 echo ""
 echo "$0: building the user's workspace"
 echo ""
 
 # this first build compiles the contents of "src" and storest the intermediate
-tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker build - --no-cache --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --file Dockerfile --platform=linux/$ARCH
+tar -czh $PASS_TO_DOCKER_BUILD 2>/dev/null | docker buildx build - --no-cache --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --build-arg ARCH=${ARCH} --file Dockerfile --platform=linux/$ARCH
 
 echo ""
 echo "$0: packing the workspace into a docker image"
@@ -41,7 +53,7 @@ echo ""
 
 # this second build takes the resulting workspace and storest in in a final image
 # that can be deployed to a drone
-docker build . --no-cache --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --tag $OUTPUT_IMAGE --platform=linux/$ARCH
+docker buildx build . --no-cache --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --build-arg BASE_IMAGE=${BASE_IMAGE} --build-arg TRANSPORT_IMAGE=${TRANSPORT_IMAGE} --build-arg ARCH=${ARCH} --tag $OUTPUT_IMAGE --platform=linux/$ARCH
 
 echo ""
 echo "$0: workspace was packed into '$OUTPUT_IMAGE'"
