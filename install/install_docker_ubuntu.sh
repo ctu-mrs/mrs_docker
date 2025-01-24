a3a7a06d2cef1bb53639bd30d61f10568a5044bf
Tomas Baca <Tomas Baca>
2025-01-24 13:14:08 +0100

    updated builders

--- install/install_docker_ubuntu.sh
+++ install/install_docker_ubuntu.sh
@@ -38,7 +38,7 @@ sudo apt-get -y update
 
 ## | ------------------ install docker engine ----------------- |
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin qemu-user-static
 
 sudo usermod -aG docker $USER
 
--- recipes/ros_noetic/build_image.sh
+++ recipes/ros_noetic/build_image.sh
@@ -16,29 +16,12 @@ REGISTRY=ctumrs
 # single-platform image can be stored locally
 ARCH=linux/amd64
 # ARCH=linux/arm64
-OUTPUT="--output type=docker"
-
-# multi-platform image can not be stored locally, needs to be pushed
-# ARCH=linux/arm64,linux/amd64
-# OUTPUT="--push"
 
 ## --------------------------------------------------------------
 ## |                            build                           |
 ## --------------------------------------------------------------
 
-# multiplatform builder
-BUILDER=container-builder
-
-# get info about an existing builder
-container_builder_info=$(docker buildx inspect ${BUILDER})
-
-if [[ "$?" == "0" ]]; then
-  # activate the builder if it exists
-  docker buildx use ${BUILDER}
-else
-  # create the builder if it does not exist
-  docker buildx create --name ${BUILDER} --driver docker-container --bootstrap --use
-fi
+docker buildx use default
 
 # build the docker image using the builder and export the results to the local docker registry
 docker buildx build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --platform=${ARCH} ${OUTPUT}
