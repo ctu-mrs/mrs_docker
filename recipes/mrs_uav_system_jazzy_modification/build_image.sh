5e013a5e3bd74b2e2ddd017e8821ee91a81b6437
Tomas Baca <Tomas Baca>
2025-08-18 17:45:44 +0200

    minor changes to lazydocker deployment

--- recipes/mrs_uav_system_jazzy_modification/build_image.sh
+++ recipes/mrs_uav_system_jazzy_modification/build_image.sh
@@ -10,26 +10,12 @@ cd ${MY_PATH}
 ## |                            setup                           |
 ## --------------------------------------------------------------
 
-LOCAL_TAG=mrs_uav_system:unstable
+LOCAL_TAG=mrs_uav_system:unstable_modified
 REGISTRY=ctumrs
 
 ## --------------------------------------------------------------
 ## |                            build                           |
 ## --------------------------------------------------------------
 
-# multiplatform builder
-BUILDER=container-builder
-
-# get info about an existing builder
-container_builder_info=$(docker buildx inspect ${BUILDER})
-
-if [[ "$?" == "0" ]]; then
-  # activate the builder if it exists
-  docker buildx use ${BUILDER}
-else
-  # create the builder if it does not exist
-  docker buildx create --name ${BUILDER} --driver docker-container --bootstrap --use
-fi
-
 # build the docker image using the builder and export the results to the local docker registry
-docker buildx build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG  --platform=linux/amd64 --push
+docker build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --platform=linux/amd64
