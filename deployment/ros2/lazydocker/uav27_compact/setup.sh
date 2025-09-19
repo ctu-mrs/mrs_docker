e54e493c939241b495a081df2642440f657419f0
Tomas Baca <Tomas Baca>
2025-09-19 07:37:20 +0200

    removed unused sessions, updated record scripts

--- deployment/ros2/lazydocker/uav27_compact/setup.sh
+++
@@ -1,4 +0,0 @@
-# - defines the name for the docker compose session
-# - rosbags are placed to ~/docker/$SESSION_NAME/bag_files
-# - terminal logs are placed to ~/docker/$SESSION_NAME/logs
-export SESSION_NAME=just_flying
